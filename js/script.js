ae7072e18b10ca7c5384bccb65af77070091cee8
ReviArda <149865233+ReviArda@users.noreply.github.com>
2023-11-04 15:06:35 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika tugas menu di klik
+document.querySelector("#tugas-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const tugas = document.querySelector("#tugas-menu");
+
+document.addEventListener("click", function (e) {
+  if (!tugas.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
